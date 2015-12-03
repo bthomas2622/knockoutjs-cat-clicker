5b2401d3a025d3bd9a78e901e246c90adfa46065
Ben Thomas <bthomas2622@gmail.com>
2015-12-02 21:46:58 -0500

    changed catmodel to accept variable data into function to allow unique cat attributes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-var CatModel = function(){
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Cassius Clay');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+var CatModel = function(data){
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
 	this.level = ko.computed(function() {
 		if (this.clickCount() > 9){
 			return ('Lvl: Cat');
@@ -11,18 +11,18 @@ var CatModel = function(){
 		}
 	}, this);
 
-	this.people = ko.observableArray([
-		{nickname: 'Buzz'},
-		{nickname: 'Woody'},
-		{nickname: 'The Claw'}
-	]);
+	this.nicknames = ko.observableArray(data.nicknames);
 };
 
 var ViewModel = function () {
-	this.currentCat = ko.observable(new CatModel());
-
+	this.currentCat = ko.observable(new CatModel({
+		clickCount: 0,
+		name: 'Cassius Clay',
+		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+		nicknames: [{nickname: 'Buzz'}, {nickname: 'Woody'}, {nickname: 'The Claw'}]
+	}));
 	this.incrementCounter = function() {
-		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+		this.clickCount(this.clickCount() + 1);
 	};
 
 };
