0e5abfa38aa3366fcf4ada665143d221a4980dda
Ben Thomas <bthomas2622@gmail.com>
2015-12-03 20:48:10 -0500

    added list of cats to choose through with observable array

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
+var initialCats = [
+	{
+		clickCount : 0,
+		name : 'George',
+		imgSrc : 'img/kitty1.jpg',
+		nicknames : ['Foreman', 'Orwell']
+	},
+	{
+		clickCount : 0,
+		name : 'Frederick',
+		imgSrc : 'img/kitty2.jpg',	
+		nicknames : ['Sir Lancelot', 'Puss in Boots']	
+	},
+	{
+		clickCount : 0,
+		name : 'Wendy',
+		imgSrc : 'img/kitty3.jpg',		
+		nicknames : ['Frosty', 'Wittle Kitty']
+	},
+	{
+		clickCount : 0,
+		name : 'Marge',
+		imgSrc : 'img/kitty4.jpg',	
+		nicknames : ['Simpson', 'I cant believe its not butter']	
+	},
+	{
+		clickCount : 0,
+		name : 'Gambino',
+		imgSrc : 'img/kitty5.jpg',	
+		nicknames : ['Is a mastermind', 'Childish']	
+	},		
+];
+
 var CatModel = function(data){
 	this.clickCount = ko.observable(data.clickCount);
 	this.name = ko.observable(data.name);
@@ -15,14 +48,21 @@ var CatModel = function(data){
 };
 
 var ViewModel = function () {
-	this.currentCat = ko.observable(new CatModel({
-		clickCount: 0,
-		name: 'Cassius Clay',
-		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-		nicknames: [{nickname: 'Buzz'}, {nickname: 'Woody'}, {nickname: 'The Claw'}]
-	}));
+	var self = this;
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem){
+		self.catList.push(new CatModel(catItem));
+	});
+
+	this.currentCat = ko.observable(this.catList()[0]);
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
+	};
+
+	this.setCat = function(clickedCat) {
+		self.currentCat(clickedCat);
 	};
 
 };
